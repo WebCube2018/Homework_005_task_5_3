e3dca379d9202b20db541d123079e3479cb41fb4
Andrei Donets <info@webcube.su>
2020-01-12 16:43:04 +0300

    завершил ДЗ 5 задание  5.3

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
 
 require_once "vendor/autoload.php";
+
+$image = new \src\ChangeImage();
+echo $image->imageAction("php.png");
--- src/ChangeImage.php
+++ src/ChangeImage.php
@@ -1,7 +1,44 @@
 <?php
 namespace src;
 
+use Intervention\Image\Gd\Font;
+use Intervention\Image\ImageManagerStatic as Image;
+
 class ChangeImage
 {
+    protected static $imagePatch;
 
+    public function imageAction($nameFile)
+    {
+        self::$imagePatch = $_SERVER["DOCUMENT_ROOT"] . "/images/";
+
+        $file = self::$imagePatch . $nameFile;
+        $result = self::$imagePatch . "new_" . $nameFile;
+
+        $image = Image::make($file)
+            ->rotate(45)
+            ->resize(200, null, function ($image) {
+                $image->aspectRatio();
+            })
+            ->save($result, 80);
+
+        self::watermark($image);
+
+        return $image->response("png");
+    }
+
+    public static function watermark(\Intervention\Image\Image $image)
+    {
+        $image->text(
+            "loftschool",
+            $image->width() / 2,
+            $image->height() / 1.5,
+            function (Font $font) {
+                $font->file(self::$imagePatch . "arial.ttf")->size("14");
+                $font->color('#1e2a36');
+                $font->align("right");
+                $font->valign("center");
+            }
+        );
+    }
 }
